ca4d9d491836df146534f7d44390713b99d3b46b
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-05-16 22:33:21 -0300

    criando buttons do menu e metodo de mudança de cor

--- WinFormsFinancas/FormMainMenu.Designer.cs
+++ WinFormsFinancas/FormMainMenu.Designer.cs
@@ -28,31 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMainMenu));
             panelMenu = new Panel();
+            panelLogo = new Panel();
+            btnProducts = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            panelMenu.SuspendLayout();
             SuspendLayout();
             // 
             // panelMenu
             // 
             panelMenu.BackColor = Color.MidnightBlue;
+            panelMenu.Controls.Add(button5);
+            panelMenu.Controls.Add(button4);
+            panelMenu.Controls.Add(button3);
+            panelMenu.Controls.Add(button2);
+            panelMenu.Controls.Add(button1);
+            panelMenu.Controls.Add(btnProducts);
+            panelMenu.Controls.Add(panelLogo);
             panelMenu.Dock = DockStyle.Left;
             panelMenu.Location = new Point(0, 0);
             panelMenu.Name = "panelMenu";
-            panelMenu.Size = new Size(250, 450);
+            panelMenu.Size = new Size(220, 450);
             panelMenu.TabIndex = 0;
             // 
-            // Form1
+            // panelLogo
+            // 
+            panelLogo.BackColor = Color.FromArgb(0, 0, 64);
+            panelLogo.Dock = DockStyle.Top;
+            panelLogo.Location = new Point(0, 0);
+            panelLogo.Name = "panelLogo";
+            panelLogo.Size = new Size(220, 80);
+            panelLogo.TabIndex = 0;
+            // 
+            // btnProducts
+            // 
+            btnProducts.Dock = DockStyle.Top;
+            btnProducts.FlatAppearance.BorderSize = 0;
+            btnProducts.FlatStyle = FlatStyle.Flat;
+            btnProducts.ForeColor = SystemColors.GradientInactiveCaption;
+            btnProducts.Image = Properties.Resources.icons8_apresentação_16;
+            btnProducts.ImageAlign = ContentAlignment.MiddleLeft;
+            btnProducts.Location = new Point(0, 80);
+            btnProducts.Name = "btnProducts";
+            btnProducts.Padding = new Padding(12, 0, 0, 0);
+            btnProducts.Size = new Size(220, 45);
+            btnProducts.TabIndex = 1;
+            btnProducts.Text = "   Products";
+            btnProducts.TextAlign = ContentAlignment.MiddleLeft;
+            btnProducts.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnProducts.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Top;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = SystemColors.GradientInactiveCaption;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(0, 125);
+            button1.Name = "button1";
+            button1.Padding = new Padding(12, 0, 0, 0);
+            button1.Size = new Size(220, 45);
+            button1.TabIndex = 2;
+            button1.Text = "   Orders";
+            button1.TextAlign = ContentAlignment.MiddleLeft;
+            button1.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Top;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = SystemColors.GradientInactiveCaption;
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(0, 170);
+            button2.Name = "button2";
+            button2.Padding = new Padding(12, 0, 0, 0);
+            button2.Size = new Size(220, 45);
+            button2.TabIndex = 3;
+            button2.Text = "   Customer";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Dock = DockStyle.Top;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = SystemColors.GradientInactiveCaption;
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(0, 215);
+            button3.Name = "button3";
+            button3.Padding = new Padding(12, 0, 0, 0);
+            button3.Size = new Size(220, 45);
+            button3.TabIndex = 4;
+            button3.Text = "   Reporting";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Dock = DockStyle.Top;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.ForeColor = SystemColors.GradientInactiveCaption;
+            button4.Image = (Image)resources.GetObject("button4.Image");
+            button4.ImageAlign = ContentAlignment.MiddleLeft;
+            button4.Location = new Point(0, 260);
+            button4.Name = "button4";
+            button4.Padding = new Padding(12, 0, 0, 0);
+            button4.Size = new Size(220, 45);
+            button4.TabIndex = 5;
+            button4.Text = "   Notifications";
+            button4.TextAlign = ContentAlignment.MiddleLeft;
+            button4.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Dock = DockStyle.Top;
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.ForeColor = SystemColors.GradientInactiveCaption;
+            button5.Image = (Image)resources.GetObject("button5.Image");
+            button5.ImageAlign = ContentAlignment.MiddleLeft;
+            button5.Location = new Point(0, 305);
+            button5.Name = "button5";
+            button5.Padding = new Padding(12, 0, 0, 0);
+            button5.Size = new Size(220, 45);
+            button5.TabIndex = 6;
+            button5.Text = "   Setting";
+            button5.TextAlign = ContentAlignment.MiddleLeft;
+            button5.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // FormMainMenu
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
             Controls.Add(panelMenu);
-            Name = "Form1";
+            Name = "FormMainMenu";
             Text = "Form1";
+            panelMenu.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panelMenu;
+        private Panel panelLogo;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Button btnProducts;
     }
 }
--- WinFormsFinancas/FormMainMenu.cs
+++ WinFormsFinancas/FormMainMenu.cs
@@ -2,9 +2,24 @@ namespace WinFormsFinancas
 {
     public partial class FormMainMenu : Form
     {
+        private Button currentButton;
+        private Random random;
+        private int tempIndex;
         public FormMainMenu()
         {
             InitializeComponent();
+        }
+
+        private Color SelectthemeColor()
+        {
+            int index = random.Next(ThemerColor.ColorList.Count);
+            while (tempIndex == index)
+            {
+                random.Next(ThemerColor.ColorList.Count);
+            }
+            tempIndex = index;
+            string color = ThemerColor.ColorList[index];
+            return ColorTranslator.FromHtml(color); 
         }
     }
 }
